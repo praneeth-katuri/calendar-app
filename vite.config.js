609b99683fb72ae0d9abe8e7c7639f9dc3c963b1
praneeth-katuri <praneethkaturi@outlook.com>
2025-05-15 21:04:36 +0530

    setup gh-pages for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/calendar-app/",
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
